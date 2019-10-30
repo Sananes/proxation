59b1f9723a31f037450fbf780540e2c6406e27e0
Aaron Sananes <aasananes@gmail.com>
2019-10-30 08:57:04 +0000

    Added new seo section settings

--- studio/schemas/objects/seo.js
+++ studio/schemas/objects/seo.js
@@ -4,7 +4,8 @@ export default {
   type: "object",
   fields: [
     { title: "Title Tag", name: "title", type: "string" },
-    { name: "description", title: "Meta Description", type: "text" }
+    { name: "description", title: "Meta Description", type: "text" },
+    { name: "image", title: "Meta Image", type: "image" }
   ],
   options: { collapsible: true, collapsed: true }
 };
