bbb16976d30d59ed1c4a3108bdf0edd4042235e3
Aaron Sananes <aasananes@gmail.com>
2019-11-04 15:06:06 +0000

    Added new type for person

--- studio/schemas/person.js
+++ studio/schemas/person.js
@@ -1,53 +1,75 @@
-import { MdPerson } from 'react-icons/md'
+import { MdPerson } from "react-icons/md";
 
 export default {
-  name: 'person',
-  title: 'Person',
-  type: 'document',
+  name: "person",
+  title: "Person",
+  type: "document",
   icon: MdPerson,
   liveEdit: false,
   fields: [
     {
-      name: 'name',
-      title: 'Name',
-      type: 'string'
+      name: "name",
+      title: "Name",
+      type: "string"
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      description: 'Some frontend will require a slug to be set to be able to show the person',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      description: "Some frontend will require a slug to be set to be able to show the person",
       options: {
-        source: 'name',
+        source: "name",
         maxLength: 96
       }
     },
     {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "image",
+      title: "Image",
+      type: "image",
       options: {
         hotspot: true
       }
     },
     {
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
+      name: "title",
+      title: "Title",
+      type: "string",
+      description: "Role in the company. E.g. Developer"
+    },
+    {
+      name: "bio",
+      title: "Bio",
+      type: "array",
       of: [
         {
-          title: 'Block',
-          type: 'block',
-          styles: [{ title: 'Normal', value: 'normal' }],
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
           lists: []
         }
       ]
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+      options: {
+        layout: "radio",
+        list: [
+          { title: "Designer", value: "designer" },
+          { title: "Developer", value: "developer" },
+          { title: "Project Manager", value: "projectManager" },
+          { title: "Founder", value: "founder" },
+          { title: "Co-Founder", value: "co-founder" },
+          { title: "Marketing", value: "marketing" }
+        ]
+      }
     }
   ],
   preview: {
     select: {
-      title: 'name',
-      media: 'image'
+      title: "name",
+      media: "image"
     }
   }
-}
+};
