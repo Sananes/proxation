ca995b6ed7df2605bbad33676c29b7299997d2cb
Aaron Sananes <aasananes@gmail.com>
2020-01-13 13:30:15 +0000

    Added sanity plugin

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,14 @@
+export default {
+  widgets: [
+    {
+      name: "gatsby",
+      options: {
+        instances: [
+          {
+            instanceUrl: "https://proxation-3634876568.gtsb.io/"
+          }
+        ]
+      }
+    }
+  ]
+};
