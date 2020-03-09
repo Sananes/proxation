d8a133c0c57270a15ee51b625b38794fa27e32c1
Aaron Sananes <aasananes@gmail.com>
2020-03-09 14:25:34 +0000

    Updated

--- web/src/components/ContactForm/ContactForm.js
+++ web/src/components/ContactForm/ContactForm.js
@@ -113,7 +113,7 @@ const ContactForm = ({ isDark } = props) => {
               <label className={cn(styles.label)} htmlFor="budget">
                 <h3>Budget</h3>
                 <div className={styles.selectWrapper}>
-                  <select name="budget[]" className={styles.select} id="budget" ref={register}>
+                  <select name="budget[]" className={styles.select} id="budget[]" ref={register}>
                     {options &&
                       options.map(option => (
                         <option key={option.value} value={option.value} disabled={option.options}>
